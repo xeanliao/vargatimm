9423bc5be96dcb3f0d07463991369fcb2cf89873
info@whdiyo.com <info@whdiyo.com>
2016-02-16 13:34:25 +0800

    add phantomjs proxy to avoid let client access phantomjs server

---
+++ vargainc.api/Vargainc.Timm.REST/ViewModel/MapImage/MapTypeEnum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vargainc.Timm.REST.ViewModel.MapImage
+{
+    public enum MapTypeEnum
+    {
+        ROADMAP,
+        SATELLITE,
+        HYBRID,
+        TERRAIN
+    }
+}
