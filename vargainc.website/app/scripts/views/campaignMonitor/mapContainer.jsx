fd124c04ce869af573f23f1a35aa357fbcfabff3
steve <ghostuz@outlook.com>
2017-06-03 01:33:26 +0800

    fix icon offset

--- vargainc.website/app/scripts/views/campaignMonitor/mapContainer.jsx
+++ vargainc.website/app/scripts/views/campaignMonitor/mapContainer.jsx
@@ -1013,7 +1013,7 @@ export default React.createBackboneClass({
 				"layout": {
 					"icon-image": "truck",
 					"icon-size": 0.75,
-					"icon-offset": "0,-5"
+					"icon-offset": "0,-5",
 					"icon-allow-overlap": true
 				},
 				"paint": {}
