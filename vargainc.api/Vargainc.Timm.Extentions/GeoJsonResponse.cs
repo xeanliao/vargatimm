c28b39ce776180a96ab12c421c6ea62322fd1834
Steve.Yin <dev@whdiyo.com>
2022-02-22 00:21:40 +0800

    fix geojson load issue

---
+++ vargainc.api/Vargainc.Timm.Extentions/GeoJsonResponse.cs
@@ -0,0 +1,52 @@
+﻿using NetTopologySuite.Features;
+using NetTopologySuite.IO;
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+
+namespace Vargainc.Timm.Extentions
+{
+    public class GeoJsonResponse : IHttpActionResult
+    {
+        public GeoJsonResponse(FeatureCollection layers, string etag)
+        {
+            this._layers = layers;
+        }
+
+        private FeatureCollection _layers;
+        private string _eTag;
+
+        public Task<HttpResponseMessage> ExecuteAsync(CancellationToken cancellationToken)
+        {
+            MemoryStream ms = new MemoryStream();
+            var serializer = GeoJsonSerializer.Create();
+            var streamWriter = new StreamWriter(ms, Encoding.Default, 1024, true);
+
+            serializer.Serialize(streamWriter, this._layers);
+            streamWriter.Flush();
+            ms.Seek(0, SeekOrigin.Begin);
+
+            var eTag = Convert.ToBase64String(MD5.Create().ComputeHash(ms));
+
+            if (eTag == this._eTag)
+            {
+                return Task.FromResult(new HttpResponseMessage(HttpStatusCode.NotModified));
+            }
+
+            ms.Seek(0, SeekOrigin.Begin);
+            HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.OK);
+            response.Headers.TryAddWithoutValidation("ETag", eTag);
+            response.Content = new StreamContent(ms);
+            response.Content.Headers.ContentType = new MediaTypeHeaderValue("application/geo+json");
+            return Task.FromResult(response);
+        }
+    }
+}
--- vargainc.api/Vargainc.Timm.REST/Controllers/DMapController.cs
+++ vargainc.api/Vargainc.Timm.REST/Controllers/DMapController.cs
@@ -16,6 +16,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Http;
+using Vargainc.Timm.Extentions;
 using Vargainc.Timm.Models;
 using Vargainc.Timm.REST.Helper;
 using Vargainc.Timm.REST.ViewModel;
@@ -27,7 +28,7 @@ namespace Vargainc.Timm.REST.Controllers
     {
         [HttpGet]
         [Route("{campaignId}/dmap/geojson")]
-        public async Task<HttpResponseMessage> LoadDMapGeoJson(int? campaignId)
+        public async Task<IHttpActionResult> LoadDMapGeoJson(int? campaignId)
         {
             var layers = new FeatureCollection();
             var campaign = await db.Campaigns.FindAsync(campaignId).ConfigureAwait(false);
@@ -35,42 +36,31 @@ namespace Vargainc.Timm.REST.Controllers
             var subMaps = await db.SubMaps.Include(i => i.SubMapRecords).Where(i => i.CampaignId == campaignId).ToListAsync().ConfigureAwait(false);
             var subMapIds = subMaps.Select(i => i.Id).ToList();
             var subMapRecords = subMaps.SelectMany(i => i.SubMapRecords.Select(j => new { j.Classification, j.AreaId, j.SubMapId })).ToList();
-            var subMapGeom = await db.SubMapCoordinates
-                .Where(i => subMapIds.Contains(i.SubMapId))
-                .OrderBy(i => i.SubMapId)
-                .ThenBy(i => i.Id)
-                .Select(i => new { i.SubMapId, i.Longitude, i.Latitude })
-                .ToListAsync()
-                .ConfigureAwait(false);
 
             foreach (var item in subMaps)
             {
-                var coorinates = subMapGeom.Where(i => i.SubMapId == item.Id).Select(i => new Coordinate(i.Longitude ?? 0, i.Latitude ?? 0)).ToList();
-                if(coorinates.Count == 0)
+                var points = item.SubMapCoordinates.OrderBy(i => i.Id).Select(i => new Coordinate(i.Longitude ?? 0, i.Latitude ?? 0)).ToList();
+                if (points.Count == 0)
                 {
                     continue;
                 }
-
-                if (!coorinates.First().Equals2D(coorinates.Last()))
+                // close it
+                if (!points.First().Equals2D(points.Last()))
                 {
-                    coorinates.Add(coorinates.First());
+                    points.Add(points.First());
                 }
-                var geom = GeometryFactory.CreatePolygon(coorinates.ToArray());
-                geom.Normalize();
-                //if (!geom.IsValid)
-                //{
-                //    geom = geom.Buffer(0) as Polygon;
-                //}
+                var polygon = GeometryFactory.CreatePolygon(points.ToArray());
+                polygon.Normalize();
 
-                if(geom == null)
+                if (polygon == null)
                 {
                     continue;
                 }
 
                 layers.Add(new Feature
                 {
-                    Geometry = geom,
-                    BoundingBox = geom.EnvelopeInternal,
+                    Geometry = polygon,
+                    BoundingBox = polygon.EnvelopeInternal,
                     Attributes = new AttributesTable
                     {
                         { "type", "submap" },
@@ -83,6 +73,7 @@ namespace Vargainc.Timm.REST.Controllers
                 });
             }
 
+
             var dMaps = await db.DistributionMaps
                 .Where(i => subMapIds.Contains(i.SubMapId))
                 .OrderBy(i => i.SubMapId)
@@ -90,40 +81,33 @@ namespace Vargainc.Timm.REST.Controllers
                 .ToListAsync()
                 .ConfigureAwait(false);
             var dMapIds = dMaps.Select(i => i.Id).ToList();
-            var dMapGeom = await db.DistributionMapCoordinates
-                .Where(i => dMapIds.Contains(i.DistributionMapId))
-                .OrderBy(i => i.DistributionMapId)
-                .ThenBy(i => i.Id)
-                .Select(i => new { i.DistributionMapId, i.Longitude, i.Latitude })
-                .ToListAsync()
-                .ConfigureAwait(false);
 
             var dMapPolygon = new List<Polygon>();
             foreach (var item in dMaps)
             {
-                var coorinates = dMapGeom.Where(i => i.DistributionMapId == item.Id).Select(i => new Coordinate(i.Longitude ?? 0, i.Latitude ?? 0)).ToList();
-                if (coorinates.Count == 0)
+                var points = item.DistributionMapCoordinates.OrderBy(i=>i.Id).Select(i => new Coordinate(i.Longitude ?? 0, i.Latitude ?? 0)).ToList();
+                if (points.Count == 0)
                 {
                     continue;
                 }
-                if (!coorinates.First().Equals2D(coorinates.Last()))
+                // close it
+                if (!points.First().Equals2D(points.Last()))
                 {
-                    coorinates.Add(coorinates.First());
+                    points.Add(points.First());
                 }
-                var geom = GeometryFactory.CreatePolygon(coorinates.ToArray());
-                geom.Normalize();
-                if (!geom.IsValid)
-                {
-                    geom = geom.Buffer(0) as Polygon;
-                }
-                if(geom == null)
+                var polygon = GeometryFactory.CreatePolygon(points.ToArray());
+                polygon.Normalize();
+
+                if (polygon == null)
                 {
                     continue;
                 }
-                dMapPolygon.Add(geom);
+               
+                
+                dMapPolygon.Add(polygon);
                 layers.Add(new Feature
                 {
-                    Geometry = geom.Centroid,
+                    Geometry = polygon.Centroid,
                     Attributes = new AttributesTable
                     {
                         { "type", "dmap" },
@@ -139,8 +123,8 @@ namespace Vargainc.Timm.REST.Controllers
                 });
                 layers.Add(new Feature
                 {
-                    Geometry = geom,
-                    BoundingBox = geom.EnvelopeInternal,
+                    Geometry = polygon,
+                    BoundingBox = polygon.EnvelopeInternal,
                     Attributes = new AttributesTable
                     {
                         { "type", "dmap" },
@@ -176,15 +160,20 @@ namespace Vargainc.Timm.REST.Controllers
                 var boundary = GeometryFactory.CreateLineString(dMapPolygon[i].Boundary.Coordinates);
                 dMapIntersectionMultiLine = dMapIntersectionMultiLine.Union(boundary);
             }
-            layers.Add(new Feature
+
+            if (!dMapIntersectionMultiLine.IsEmpty)
             {
-                Geometry = dMapIntersectionMultiLine,
-                Attributes = new AttributesTable
+                layers.Add(new Feature
+                {
+                    Geometry = dMapIntersectionMultiLine,
+                    Attributes = new AttributesTable
                     {
                         { "type", "dmap-intersection" },
                         { "id", $"dmap-intersection" },
                     }
-            });
+                });
+            }
+            
 
             var recordGroup = subMapRecords
                 .GroupBy(i => i.Classification)
@@ -270,27 +259,8 @@ namespace Vargainc.Timm.REST.Controllers
                 sqlReader.Close();
             }
 
-            MemoryStream ms = new MemoryStream();
-            var serializer = GeoJsonSerializer.Create();
-            var streamWriter = new StreamWriter(ms, Encoding.Default, 1024, true);
-
-            serializer.Serialize(streamWriter, layers);
-            streamWriter.Flush();
-            ms.Seek(0, SeekOrigin.Begin);
-
-            var eTag = Convert.ToBase64String(MD5.Create().ComputeHash(ms));
-            var eTagCheck = GetETag();
-            if (eTag == eTagCheck)
-            {
-                return new HttpResponseMessage(HttpStatusCode.NotModified);
-            }
-
-            ms.Seek(0, SeekOrigin.Begin);
-            HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.OK);
-            response.Headers.TryAddWithoutValidation("ETag", eTag);
-            response.Content = new StreamContent(ms);
-            response.Content.Headers.ContentType = new MediaTypeHeaderValue("application/geo+json");
-            return response;
+            var eTag = GetETag();
+            return new GeoJsonResponse(layers, eTag);
         }
 
         [HttpPost]
